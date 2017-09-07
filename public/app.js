f76e96e0ccae0c2d32719049c0d51b22be852304
Jellis <venetianself@hotmail.com>
2017-09-07 14:19:34 -0500

    data from api displays

--- public/app.js
+++ public/app.js
@@ -5,12 +5,19 @@ const app = angular.module('learnSomething', []);
 app.controller('mainController', ['$http', function($http){
 	this.lessons = [];
 
+
 	$http({
 		method: 'GET',
 		url: 'http://localhost:3000/lessons'
 	}).then
 		(response => {
-			console.log(response.data);
+			this.name = response.data[0].name;
+			this.author = response.data[0].author;
+			this.date = response.data[0].date;
+			this.language = response.data[0].language;
+			this.difficulty = response.data[0].difficulty;
+			this.url = response.data[0].url;
+			console.log(response.data[0].name);
 			this.lessons = response.data;
 		}).catch(err => console.log(err));
 }]);
